3f8dd86c753ee3b1966c19b54f1f70a313eabb41
hidwoc <heidischoi@gmail.com>
2021-08-12 10:47:19 -0400

    Create basket and line_item helper functions

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,4 +1,12 @@
 class Basket < ApplicationRecord
-  has_many :line_items
+  has_many :line_items, dependent :destroy
   has_many :dishes, through :line_items
+
+  def total
+    sum = 0
+    self.line_items.each do |line_item|
+      sum+= line_item.total_price
+    end
+    return sum
+  end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ApplicationRecord
   belongs_to :dish
   belongs_to :basket
+
+  def total_price
+    self.quantity * self.dish.price
+  end
 end
